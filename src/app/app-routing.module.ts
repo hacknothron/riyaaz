b49d5b15ac1d055b1eba3656cd29808cd32a7c8f
Shivam Rathod <rathod.shivam289@gmail.com>
2019-08-10 15:51:58 +0530

    Shivam: routing for compunent in last commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,38 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
-import { LandingComponent } from './landing/landing.component';
+import { NgModule } from "@angular/core";
+import { Routes, RouterModule } from "@angular/router";
+import { LandingComponent } from "./landing/landing.component";
+import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component';
+import { EventsComponent } from './events/events.component';
+import { FacultyComponent } from './faculty/faculty.component';
+import { TeamComponent } from './team/team.component';
 
 const routes: Routes = [
   {
-    path:'',
-    pathMatch:'full',
-    component:LandingComponent
+    path: "",
+    pathMatch: "full",
+    component: LandingComponent
+  },
+  {
+    path: "/aboutus",
+    component: AboutComponent
+  },
+  {
+    path: "/contactus",
+    component: ContactComponent
+  },
+  {
+    path: "/event",
+    component: EventsComponent
+  },
+  {
+    path: "/faculty",
+    component: FacultyComponent
+  },
+  
+  {
+    path: "/team",
+    component: TeamComponent
   }
 ];
 
@@ -14,4 +40,4 @@ const routes: Routes = [
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
