30625ef654368d6eadb2ef492d2811ea7b659262
Shivam Rathod <rathod.shivam289@gmail.com>
2019-08-10 15:46:46 +0530

    Shivam: about us, contact, classses, events, faculty, team components creating

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LandingComponent } from './landing/landing.component';
+import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component';
+import { ClassesComponent } from './classes/classes.component';
+import { EventsComponent } from './events/events.component';
+import { FacultyComponent } from './faculty/faculty.component';
+import { TeamComponent } from './team/team.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LandingComponent,
+    AboutComponent,
+    ContactComponent,
+    ClassesComponent,
+    EventsComponent,
+    FacultyComponent,
+    TeamComponent
   ],
   imports: [
     BrowserModule,
